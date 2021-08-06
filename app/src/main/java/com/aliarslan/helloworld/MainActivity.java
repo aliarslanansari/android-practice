e44a3a8b45599fb3532d7d624f406aa808a950af
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-06 23:42:11 +0530

    Added button to change text

--- app/src/main/java/com/aliarslan/helloworld/MainActivity.java
+++ app/src/main/java/com/aliarslan/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package com.aliarslan.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View view){
+        TextView textViewHello = findViewById(R.id.textMessage);
+        textViewHello.setText("Hello ALI");
     }
 }
