f1add74192e562d02af604b4873d470b46f1ad38
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-07 23:51:04 +0530

    tried progress bar, checkbox, radio button

--- app/src/main/java/com/aliarslan/helloworld/MainActivity.java
+++ app/src/main/java/com/aliarslan/helloworld/MainActivity.java
@@ -3,34 +3,75 @@ package com.aliarslan.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    int i = 0;
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btnHello:
-                Toast.makeText(this,"Hello Button Clicked",Toast.LENGTH_SHORT).show();
-                break;
-            default:
-        }
-    }
-
+public class MainActivity extends AppCompatActivity {
+    private CheckBox checkBoxHarry, checkBoxMatrix, checkBoxJoker;
+    private RadioGroup radioGroupMaritalStatus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnHello = findViewById(R.id.btnHello);
-        btnHello.setOnClickListener(this);
-        btnHello.setOnLongClickListener(new View.OnLongClickListener() {
+        checkBoxHarry = findViewById(R.id.checkboxHarry);
+        checkBoxJoker = findViewById(R.id.checkboxJoker);
+        checkBoxMatrix = findViewById(R.id.checkboxMatrix);
+        radioGroupMaritalStatus = findViewById(R.id.radioButtonGroup);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+
+        Thread thread = new Thread(new Runnable() {
             @Override
-            public boolean onLongClick(View v) {
-                Toast.makeText(MainActivity.this, "Long Pressed", Toast.LENGTH_SHORT).show();
-                return true;
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    progressBar.incrementProgressBy(10);
+                    SystemClock.sleep(500);
+                }
+            }
+        });
+        thread.start();
+
+        radioGroupMaritalStatus.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radioButton1:
+                        Toast.makeText(MainActivity.this, "Hel111111111", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioButton2:
+                        Toast.makeText(MainActivity.this, "Hell222222", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioButton3:
+                        Toast.makeText(MainActivity.this, "Hel3333333", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+
+        if(checkBoxHarry.isChecked()){
+            Toast.makeText(MainActivity.this, "You've watched harry potter", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(MainActivity.this, "You need to watch harry potter", Toast.LENGTH_SHORT).show();
+        }
+
+        checkBoxHarry.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(MainActivity.this, "You've watched harry potter", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "YOu need to watch harry potter", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
