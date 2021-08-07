85fd47961d585a7f4cabe6b797f96aa5ff0b987a
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-07 12:33:34 +0530

    Added button click listeners

--- app/src/main/java/com/aliarslan/helloworld/MainActivity.java
+++ app/src/main/java/com/aliarslan/helloworld/MainActivity.java
@@ -4,19 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    int i = 0;
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnHello:
+                Toast.makeText(this,"Hello Button Clicked",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnHello = findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(this);
+        btnHello.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this, "Long Pressed", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 
-    public void onBtnClick(View view){
-        TextView textViewHello = findViewById(R.id.textMessage);
-        textViewHello.setText("Hello ALI");
-    }
+
 }
