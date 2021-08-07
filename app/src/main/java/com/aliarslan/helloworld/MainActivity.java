cd4570b1223ead79c2849a577f2d843c2e2db5f6
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-08 01:50:31 +0530

    ListView and Spinner

--- app/src/main/java/com/aliarslan/helloworld/MainActivity.java
+++ app/src/main/java/com/aliarslan/helloworld/MainActivity.java
@@ -3,75 +3,70 @@ package com.aliarslan.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.RadioGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    private CheckBox checkBoxHarry, checkBoxMatrix, checkBoxJoker;
-    private RadioGroup radioGroupMaritalStatus;
+    private ListView citiesList;
+    private Spinner studentSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkBoxHarry = findViewById(R.id.checkboxHarry);
-        checkBoxJoker = findViewById(R.id.checkboxJoker);
-        checkBoxMatrix = findViewById(R.id.checkboxMatrix);
-        radioGroupMaritalStatus = findViewById(R.id.radioButtonGroup);
-        ProgressBar progressBar = findViewById(R.id.progressBar);
+        citiesList = findViewById(R.id.listItem);
+        studentSpinner = findViewById(R.id.studentSpinner);
 
-        Thread thread = new Thread(new Runnable() {
+        final ArrayList<String> studentArrayList = new ArrayList<>();
+        studentArrayList.add("Ali");
+        studentArrayList.add("Durgesh");
+        studentArrayList.add("Shlok");
+        studentArrayList.add("Tilesh");
+        studentArrayList.add("Harsh");
+
+        ArrayAdapter<String> studentArrayAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_dropdown_item,studentArrayList
+        );
+
+        studentSpinner.setAdapter(studentArrayAdapter);
+
+
+        studentSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void run() {
-                for (int i = 0; i < 10; i++) {
-                    progressBar.incrementProgressBy(10);
-                    SystemClock.sleep(500);
-                }
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, studentArrayList.get(position) + " Selected", Toast.LENGTH_SHORT).show();
+
             }
-        });
-        thread.start();
 
-        radioGroupMaritalStatus.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId){
-                    case R.id.radioButton1:
-                        Toast.makeText(MainActivity.this, "Hel111111111", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.radioButton2:
-                        Toast.makeText(MainActivity.this, "Hell222222", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.radioButton3:
-                        Toast.makeText(MainActivity.this, "Hel3333333", Toast.LENGTH_SHORT).show();
-                        break;
-                    default:
-                        break;
-                }
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(MainActivity.this, "Nothing Selected", Toast.LENGTH_SHORT).show();
+
             }
         });
 
+        final ArrayList<String> cities = new ArrayList<>();
+        cities.add("Mumbai");
+        cities.add("Pune");
+        cities.add("Dhule");
+        cities.add("Malegaon");
+        cities.add("Nashik");
+        cities.add("Aurangabad");
+        cities.add("Aurangabad");
 
-        if(checkBoxHarry.isChecked()){
-            Toast.makeText(MainActivity.this, "You've watched harry potter", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(MainActivity.this, "You need to watch harry potter", Toast.LENGTH_SHORT).show();
-        }
+        ArrayAdapter<String> citiesArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cities);
 
-        checkBoxHarry.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        citiesList.setAdapter(citiesArrayAdapter);
+
+        citiesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    Toast.makeText(MainActivity.this, "You've watched harry potter", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "YOu need to watch harry potter", Toast.LENGTH_SHORT).show();
-                }
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, cities.get(position) + " Selected", Toast.LENGTH_SHORT).show();
             }
         });
     }
